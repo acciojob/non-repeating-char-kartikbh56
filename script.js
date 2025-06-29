3b7a21326167edcad430934582f773f9ab9b972d
Kartik B H <95575463+kartikbh56@users.noreply.github.com>
2025-06-29 20:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const freq = {}
+	let n = str.length
+	for(let i=0;i<n;i++){
+		let char = str.charAt(i)
+		if(freq[char]){
+			freq[char] = freq[char] + 1
+		} else {
+			freq[char] = 1
+		}
+	}
+	for([key, value] of Object.entries(freq)) {
+		if(value===1)
+			return key
+	}
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
